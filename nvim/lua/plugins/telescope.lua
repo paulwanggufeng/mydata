52da92b049ad5f99ef01688ffd7cf03941b45ced
PaulWang <wangbc@example.com>
2023-02-08 13:43:58 +0800

    add nvim config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,32 @@
+local builtin = require('telescope.builtin')
+
+-- 进入telescope页面会是插入模式，回到正常模式就可以用j和k来移动了
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})  -- 环境里要安装ripgrep
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- You dont need to set any of these options. These are the default ones. Only
+-- the loading is important
+require('telescope').setup {
+  defaults = {
+	  previewer = true,
+	  file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+	  grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+	  qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+	},
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
+
