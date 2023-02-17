d3885ae7942e71701bb0d090a30ebd2dccd7c24a
PaulWang <wangbc@example.com>
2023-02-17 09:26:53 +0800

    add fish config file and variables

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias vi nvim
+    alias lg "/usr/local/bin/lazygit"   
+end
+
+
+### LANGUAGE
+# for lazygit
+set -gx LC_ALL en_US.UTF-8
+# set -gx LANGE en_US.UTF-8
+# for ls
+set -gx LANGUAGE en_US:en
+
