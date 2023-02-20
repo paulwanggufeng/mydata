8d6e9958e3447c871e830d81c8a78ab0820055d1
PaulWang <pwang@lindenshore.com>
2023-02-20 21:42:36 +0800

    update fish config file (split config to command and special) common config is config.fish, the special configs are place in the directory 'conf.d'

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias egrep='egrep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias grep='grep --color=auto'
+    alias ls='ls --color=auto'
+    alias ll='ls -l --color=auto --time-style=+"%Y-%m-%d %H:%M:%S"'
+    alias l.='ls -a --color=auto'
+    alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'
+
     alias vi nvim
     alias lg "/usr/local/bin/lazygit"   
 end
 
+# set -x CLANG_FORMAT_TOOL /opt/tools/clang-format
 
 ### LANGUAGE
 # for lazygit
@@ -12,5 +21,8 @@ set -gx LC_ALL en_US.UTF-8
 # for ls
 set -gx LANGUAGE en_US:en
 
+
 set -gx DENO_INSTALL $HOME/.deno
+
+fish_add_path /usr/local/go/bin
 
