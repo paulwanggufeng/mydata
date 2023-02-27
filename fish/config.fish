1ee208c9d193339f13ced180c7cb421d94a28662
PaulWang <wangbc@example.com>
2023-02-27 12:34:52 +0800

    update fish config file

--- fish/config.fish
+++ fish/config.fish
@@ -6,24 +6,20 @@ if status is-interactive
     alias ls='ls --color=auto'
     alias ll='ls -l --color=auto --time-style=+"%Y-%m-%d %H:%M:%S"'
     alias l.='ls -a --color=auto'
-    alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'
 
     alias vi nvim
-    alias lg lazygit
+    alias lg "set -x LC_ALL en_US.UTF-8; lazygit"
 end
 
-# set -x CLANG_FORMAT_TOOL /opt/tools/clang-format
+# LSP 可能会打开很多文件
+ulimit -n 32768
 
 ### LANGUAGE
-# for lazygit
-set -gx LC_ALL en_US.UTF-8
 # set -gx LANGE en_US.UTF-8
 # for ls
 set -gx LANGUAGE en_US:en
 
 
 set -gx DENO_INSTALL $HOME/.deno
-
-# fish_add_path -U ~/.local/bin/
 
 
