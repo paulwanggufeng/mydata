a224825322b50a144f7282ddc038bca03a518921
PaulWang <wangbc@example.com>
2023-03-07 09:29:49 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,22 @@
-### LANGUAGE
-# do not set in global
-# for lazygit
-# set -gx LANG en_US.UTF-8
-# for ls
-# set -x LANGUAGE en_US:en
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias egrep='egrep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias grep='grep --color=auto'
-    alias ls "set -x LANGUAGE en_US.UTF-8; /usr/bin/ls --color=auto"
-    alias ll "set -x LANGUAGE en_US.UTF-8; /usr/bin/ls -l --color=auto --time-style=+'%Y-%m-%d %H:%M:%S'"
-    alias l. "set -x LANGUAGE en_US.UTF-8; /usr/bin/ls -a --color=auto"
+  # set LANG for interactive terminal (we don't the man, lazygit show other language in the terminal)
+  # set -x LC_ALL en_US:en
+  set -x LANG en_US:en
 
-    alias vi nvim
-    alias lg "set -x LC_ALL en_US.UTF-8; lazygit"
+  # Commands to run in interactive sessions can go here
+  alias egrep='egrep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias grep='grep --color=auto'
+  alias ls "ls --color=auto"
+  alias ll "ls -l --color=auto --time-style=+'%Y-%m-%d %H:%M:%S'"
+  alias l. "ls -a --color=auto"
+
+  #alias vi nvim
+  alias vi lvim
+  alias lg lazygit
+
+  #set -x VISUAL nvim
+  set -x VISUAL lvim
 end
 
 # LSP 可能会打开很多文件
