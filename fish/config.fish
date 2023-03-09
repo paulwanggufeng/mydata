1718254de3ef88161019dc328f5fc8ee566cf51c
PaulWang <pwang@lindenshore.com>
2023-03-09 18:02:43 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,16 @@
 if status is-interactive
   # set LANG for interactive terminal (we don't the man, lazygit show other language in the terminal)
-  # set -x LC_ALL en_US:en
-  set -x LANG en_US:en
+  # set -x LC_ALL en_US:UTF-8
+  set -x LANG en_US.UTF-8
+  set -x LANGUAGE en_US:en
 
   # Commands to run in interactive sessions can go here
   alias egrep='egrep --color=auto'
   alias fgrep='fgrep --color=auto'
   alias grep='grep --color=auto'
-  alias ls "ls --color=auto"
-  alias ll "ls -l --color=auto --time-style=+'%Y-%m-%d %H:%M:%S'"
-  alias l. "ls -a --color=auto"
+  alias ls 'ls --color=auto'
+  alias ll 'ls -l --color=auto --time-style=+"%Y-%m-%d %H:%M:%S"'
+  alias l. 'ls -a --color=auto'
 
   #alias vi nvim
   alias vi lvim
