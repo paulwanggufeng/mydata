607e5367c480eb0a2af15a033e28d24330ea36bb
PaulWang <wangbc@example.com>
2023-04-02 16:59:48 +0800

    add todo-comments plugin

--- nvim/lua/plugins/plugins-setup.lua
+++ nvim/lua/plugins/plugins-setup.lua
@@ -29,7 +29,7 @@ return require('packer').startup(function(use)
   use { "folke/which-key.nvim", }
 
   use {
-    'nvim-lualine/lualine.nvim',                              -- 状态栏
+    'nvim-lualine/lualine.nvim',                             -- 状态栏
     requires = { 'nvim-tree/nvim-web-devicons', opt = true } -- 状态栏图标
     -- requires = { 'kyazdani42/nvim-web-devicons', opt = true } -- 状态栏图标
   }
@@ -137,6 +137,18 @@ return require('packer').startup(function(use)
     requires = "nvim-tree/nvim-web-devicons",
     config = function()
       require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
